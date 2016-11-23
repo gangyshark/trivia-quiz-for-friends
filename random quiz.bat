beed2c58c7d63d38e1708a6de7de7632e3e664b5
gangyshark <WHarris.harris76@gmail.com>
2016-11-22 21:06:29 -0600

    Adds quiz to repo.

---
+++ random quiz.bat
@@ -0,0 +1,391 @@
+@echo off
+:title1
+color B2
+title Random Questions
+echo.
+echo I welcome You to....
+echo.
+echo "The Random Quiz"
+echo.
+echo These questions range from history questions to science questions to 
+something else.
+echo.
+echo shall we get started?
+echo.
+echo click any button to continue
+pause>nul
+cls
+echo.
+echo INSTRUCTIONS
+echo.
+echo To play just press 1,2,3,or 4 then press ENTER to answer the question 
+echo If you get it correct you get to continue on, If you get it wrong then you have to Start all over by resterting the Game
+pause>nul
+cls
+echo First Question: How Many Engines Did the Titanic Have?
+echo 1) 2
+echo 2) 5
+echo 3) 3
+echo 4) 4
+set /p type= 
+if %type%== 4 goto correct1
+if %type%== 1 goto wrong1
+if %type%== 2 goto wrong1
+if %type%== 3 goto wrong1
+:correct1
+cls
+color 1C
+echo Correct! press any button to continue
+pause
+cls
+color B2
+echo Second Question: How many Countries are bigger than Alaska?
+echo 1) 5
+echo 2) 8
+echo 3) 12
+echo 4) 16
+set /p type= 
+if %type%== 3 goto correct1
+if %type%== 1 goto wrong1
+if %type%== 4 goto wrong1
+if %type%== 2 goto wrong1
+:correct1
+cls
+color 1C
+echo Correct! press any button to continue
+pause
+cls
+color B2
+echo Third Question: What is the sixth biggest country?
+echo 1) Greenland
+echo 2) Austrialia
+echo 3) Brazil
+echo 4) Mongolia
+set /p type= 
+if %type%== 2 goto correct1
+if %type%== 1 goto wrong1
+if %type%== 4 goto wrong1
+if %type%== 3 goto wrong1
+:correct1
+cls
+color 1C
+echo Correct! press any button to continue
+pause
+cls
+color B2
+echo Fourth Question: What is Sodium's chemicle symbol?
+echo 1) NA
+echo 2) SM
+echo 3) SO
+echo 4) W
+set /p type= 
+if %type%== 1 goto correct1
+if %type%== 2 goto wrong1
+if %type%== 4 goto wrong1
+if %type%== 3 goto wrong1
+:correct1
+cls
+color 1C
+echo Correct! press any button to continue
+pause
+cls
+color B2
+echo Fifth Question: What movie has won the most oscars?
+echo 1) Ben Hur
+echo 2) Titanic
+echo 3) The Lord of The Rings: The return of the king
+echo 4) All of The Above
+set /p type= 
+if %type%== 4 goto correct1
+if %type%== 2 goto wrong1
+if %type%== 3 goto wrong1
+if %type%== 1 goto wrong1
+:correct1
+cls
+color 1C
+echo Correct! press any button to continue
+pause
+cls
+color B2
+echo Sixth Question: How many Keys are on a piano?
+echo 1) 72
+echo 2) 89
+echo 3) 73
+echo 4) 88
+set /p type= 
+if %type%== 4 goto correct1
+if %type%== 1 goto wrong1
+if %type%== 2 goto wrong1
+if %type%== 3 goto wrong1
+:correct1
+cls
+color 1C
+echo Correct! press any button to continue
+pause
+cls
+color B2
+echo Sixth Question: What is the world's oldest building(s)
+echo 1) The Pyrimids
+echo 2) Barnenez
+echo 3) Wiebbe Hayes Stone Fort
+echo 4) Sechin Bajo
+set /p type= 
+if %type%== 2 goto correct1
+if %type%== 1 goto wrong1
+if %type%== 4 goto wrong1
+if %type%== 3 goto wrong1
+:correct1
+cls
+color 1C
+echo Correct! press any button to continue
+pause
+cls
+color B2
+echo Seventh Question: Whats the most populated city in the world?
+echo 1) New York City
+echo 2) Bejing
+echo 3) Shanghai
+echo 4) Karachi
+set /p type= 
+if %type%== 3 goto correct1
+if %type%== 2 goto wrong1
+if %type%== 1 goto wrong1
+if %type%== 4 goto wrong1
+:correct1
+cls
+color 1C
+echo Correct! press any button to continue
+pause
+cls
+color B2
+echo Eigth Question: Who was killed to start World War one?
+echo 1) ArchDuke Hanz Ferdinand
+echo 2) Prince William of Orange
+echo 3) Archduke Frank of Orange
+echo 4) Archduke Franz Ferdinand
+set /p type= 
+if %type%== 4 goto correct1
+if %type%== 1 goto wrong1
+if %type%== 2 goto wrong1
+if %type%== 3 goto wrong1
+:correct1
+cls
+color 1C
+echo Correct! press any button to continue
+pause
+cls
+color B2
+echo Ninth Question: How old is Germany?
+echo 1) 144
+echo 2) 1543
+echo 3) 2078
+echo 4) WHAT THE HELL IS A GERMANY?
+set /p type= 
+if %type%== 1 goto correct1
+if %type%== 2 goto wrong1
+if %type%== 4 goto wrong1
+if %type%== 3 goto wrong1
+:correct1
+cls
+color 1C
+echo Correct! press any button to continue
+pause
+cls
+color B2
+echo Tenth Question: Which fish is actually a mammal
+echo 1) Shark
+echo 2) Dolphin
+echo 3) Troat
+echo 4) Sting Ray
+set /p type= 
+if %type%== 2 goto correct1
+if %type%== 3 goto wrong1
+if %type%== 4 goto wrong1
+if %type%== 1 goto wrong1
+:correct1
+cls
+color 1C
+echo Correct! press any button to continue
+pause
+cls
+color B2
+echo Eleventh Question: Who made the song, "Africa?"
+echo 1) Bob Marly
+echo 2) Beatles
+echo 3) Steelers Wheeler
+echo 4) Toto
+set /p type= 
+if %type%== 4 goto correct1
+if %type%== 2 goto wrong1
+if %type%== 3 goto wrong1
+if %type%== 1 goto wrong1
+:correct1
+cls
+color 1C
+echo Correct! press any button to continue
+pause
+cls
+color B2
+echo Twelve Question: Besides Armstrong and Edwin, who was the third on Apollo 11?
+echo 1) Collins
+echo 2) Horowits
+echo 3) Jett
+echo 4) Kregel
+set /p type= 
+if %type%== 1 goto correct1
+if %type%== 2goto wrong1
+if %type%== 4 goto wrong1
+if %type%== 3 goto wrong1
+:correct1
+cls
+color 1C
+echo Correct! press any button to continue
+pause
+cls
+color B2
+echo Thirteen Question: Which of these Animals is not Extinct?
+echo 1) Saber Tooth Tiger
+echo 2) Giant Sloth
+echo 3) Amur Leopard
+echo 4) Atlas Bear
+set /p type= 
+if %type%== 3 goto correct1
+if %type%== 1 goto wrong1
+if %type%== 4 goto wrong1
+if %type%== 2 goto wrong1
+:correct1
+cls
+color 1C
+echo Correct! press any button to continue
+pause
+cls
+color B2
+echo Fourteenth Question: How Many Shoot's were fired at Kennedy?
+echo 1) 1
+echo 2) 2
+echo 3) 3
+echo 4) 4
+set /p type= 
+if %type%== 3 goto correct1
+if %type%== 1 goto wrong1
+if %type%== 4 goto wrong1
+if %type%== 2 goto wrong1
+:correct1
+cls
+color 1C
+echo Correct! press any button to continue
+pause
+cls
+color B2
+echo Fifteenth Question: How many oscars did Alfred Hitchcock win?
+echo 1) 0
+echo 2) 1
+echo 3) 2
+echo 4) 4
+set /p type= 
+if %type%== 1 goto correct1
+if %type%== 2 goto wrong1
+if %type%== 4 goto wrong1
+if %type%== 3 goto wrong1
+:correct1
+cls
+color 1C
+echo Correct! press any button to continue
+pause
+cls
+color B2
+echo SixTeenth Question: In what year did the American Revolutionary war end?
+echo 1) 1780
+echo 2) 1781
+echo 3) 1782
+echo 4) 1783
+set /p type= 
+if %type%== 4 goto correct1
+if %type%== 2 goto wrong1
+if %type%== 3 goto wrong1
+if %type%== 1 goto wrong1
+:correct1
+cls
+color 1C
+echo Correct! press any button to continue
+pause
+cls
+color B2
+echo Seventeenth Question: Who won the Very First world Series "TROPHEY?"
+echo 1) Boston Red Sox
+echo 2) St. Louis Cardinals
+echo 3) New York Yankees
+echo 4) Chicago Cubs
+set /p type= 
+if %type%== 2 goto correct1
+if %type%== 1 goto wrong1
+if %type%== 4 goto wrong1
+if %type%== 3 goto wrong1
+:correct1
+cls
+color 1C
+echo Correct! press any button to continue
+pause
+cls
+color B2
+echo Eighteenth Question: What Movie is currently at the top of the IMDB movie list?
+echo 1) Forest Gump
+echo 2) The God Father
+echo 3) The Shawshank Redemption
+echo 4) both B & C
+set /p type= 
+if %type%== 4 goto correct1
+if %type%== 1 goto wrong1
+if %type%== 2 goto wrong1
+if %type%== 3 goto wrong1
+:correct1
+cls
+color 1C
+echo Correct! press any button to continue
+pause
+cls
+color B2
+echo Ninteenth Question: What was the name of Hitler's dog?
+echo 1) Butch
+echo 2) HiesenBurg
+echo 3) Blondi
+echo 4) Doug
+set /p type= 
+if %type%== 3 goto correct1
+if %type%== 2 goto wrong1
+if %type%== 4 goto wrong1
+if %type%== 1 goto wrong1
+:correct1
+cls
+color 1C
+echo Correct! press any button to continue
+pause
+cls
+color B2
+echo Twent Question (Final Question): What kind of Coding language does this program use?
+echo 1) Java
+echo 2) C Language
+echo 3) C++
+echo 4) Batch
+set /p type= 
+if %type%== 4 goto correct2
+if %type%== 2 goto wrong1
+if %type%== 3 goto wrong1
+if %type%== 1 goto wrong1
+:correct2
+cls
+color B2
+echo GOOD JOB YOU WON THE GAME, AND IN THE END FOUND OUT,
+echo THAT THIS WAS UTERRLY A WASTE OF YOUR TIME!!! HAHA
+pause
+cls
+:wrong1
+cls
+color 40
+echo YOU GOT IT WRONG, HAHA
+cls
+goto title1
+echo.
+echo.
+echo.
+echo.
